136e665d8a4be2f8d30f3da9c3ccb0f67e424f81
jnanaprakash <prakash143.thinkspace@gmail.com>
2024-03-24 17:38:47 +0530

    Bootstrap and Scss added

--- src/index.js
+++ src/index.js
@@ -1,6 +1,11 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
-import './index.css';
+import 'bootstrap/dist/css/bootstrap.min.css';
+import $ from 'jquery';
+import Popper from 'popper.js';
+import 'bootstrap/dist/js/bootstrap.bundle.min';
+import 'bootstrap/dist/js/bootstrap.bundle.js'
+import './index.scss';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
 
